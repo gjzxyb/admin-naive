c59744f137cdd27d64fc886ed977ef6b61b33856
gjzxyb <gjzxyb@126.com>
2022-05-31 23:48:11 +0800

    update

--- src/layout/AppHeader/ToggleSidebar.vue
+++ src/layout/AppHeader/ToggleSidebar.vue
@@ -1,4 +1,5 @@
 <template>
+  <el-icon><Expand /></el-icon>
   <i :class="collapseIcon" @click="handleCollapse"></i>
 </template>
 
--- types/components.d.ts
+++ types/components.d.ts
@@ -6,16 +6,9 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElAside: typeof import('element-plus/es')['ElAside']
-    ElAutocomplete: typeof import('element-plus/es')['ElAutocomplete']
-    ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
-    ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElDropdown: typeof import('element-plus/es')['ElDropdown']
-    ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
-    ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElLink: typeof import('element-plus/es')['ElLink']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']

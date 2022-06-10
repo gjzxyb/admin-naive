0b2fee6b71006ae76ef4642dfd2be7facb3814d5
snsn100 <gjzxyb@126.com>
2022-06-10 21:57:54 +0800

    update

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,204 +1,195 @@
-import axios, { AxiosRequestHeaders, AxiosResponse } from 'axios'
-import router from '@/router'
-import qs from 'qs'
+import { merge } from '@/utils/index'
+import axios from 'axios'
+import QS from 'qs'
+import { getTokenAUTH } from '@/utils/auth'
+import { ElLoading, ElMessage } from 'element-plus'
 
-// baseURL
-const BASE_URL = import.meta.env.VITE_API_BASEURL
-const service = axios.create({
-  // API 请求的默认前缀
-  baseURL: BASE_URL,
-  timeout: 30_000, // 请求超时时间
-})
-
-const AUTH_HEADER_KEY = 'Authorization'
-const TOKEN_KEY = 'app-token'
-
-const auth = {
-  getToken(): string | null {
-    return localStorage.getItem(TOKEN_KEY)
+const IS_USE_GLOBAL_MOCK = true // 是否全部用mock数据  true是  false否
+const MOCK_SERVER_IP = 'http://localhost:8888/api'
+// baseURL会给每个接口都加上对应前缀，而项目实际场景中，
+// 存在一个前端工程，对应多个服务的场景。需要通过不用的前缀代理到不同的服务，故使用自定义prefixUrl
+const DEFAULT_PREFIX_URL =
+  process.env.NODE_ENV == 'development' ? 'http://dev.123dailu.com' : 'http://pro.123dailu.com'
+const DEFAULT_AXIOS_CONFIG = {
+  withCredentials: true, // 允许把cookie传递到后台
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json; charset=utf-8',
   },
-  setToken(token: string) {
-    localStorage.setItem(TOKEN_KEY, token)
-  },
-  clearToken() {
-    localStorage.removeItem(TOKEN_KEY)
-  },
+  timeout: 10000,
 }
 
-// 添加请求拦截器
-service.interceptors.request.use((config) => {
-  // 让每个请求携带自定义 token 请根据实际情况自行修改
-  if (auth.getToken())
-    (config.headers as AxiosRequestHeaders)[AUTH_HEADER_KEY] = 'Bearer ' + auth.getToken()
+const contentTypes = {
+  json: 'application/json; charset=utf-8',
+  urlencoded: 'application/x-www-form-urlencoded; charset=utf-8',
+  multipart: 'multipart/form-data',
+}
 
-  // 只针对get方式进行序列化
-  if (config.method === 'get')
-    config.paramsSerializer = (params) => qs.stringify(params, { arrayFormat: 'repeat' })
-
-  return config
-})
-
-// 添加响应拦截器
-service.interceptors.response.use((response) => {
-  // 检查是否携带有新的token
-  const newToken = response.headers[AUTH_HEADER_KEY]
-  if (newToken) auth.setToken(newToken)
-
-  // 如果请求成功，则重置心跳定时器
-  if (response.status === 200) resetPingTimer()
-
-  // 如果返回的自定义状态码为 4001， 则token过期，需要清理掉token并跳转至登录页面重新登录
-  if (response.data && response.data.code === 4001) {
-    auth.clearToken()
-    router.push({ name: 'Login' }).finally()
-    throw new Error('登录过期，请重新登录')
+const LoadingInstance = {
+  _target: null, // 保存Loading实例
+  _count: 0,
+}
+const pendingRequestMap = new Map()
+const fetch = (
+  url,
+  {
+    method = 'get',
+    params = {}, // 参数，根据method封装参数形式
+    axiosConfig = {}, // 自定义http配置 如responseType、headers
+    prefixUrl = '', // 自定义url前缀
+    repeat_request = false, // 允许重复请求
+    paramsType = 'json',
+    loading = true,
+    loadingOptions = {
+      // elementUI loading配置
+      text: '动静有时，大音希声',
+    },
   }
-
-  return response
-})
-
-// token 自动刷新（发送心跳）的时间间隔（分钟）
-const TOKEN_REFRESH_EXPIRE = 10
-// 心跳计时器
-let pingTimer: NodeJS.Timeout
-resetPingTimer()
-
-/**
- * 重置心跳定时器
- */
-function resetPingTimer() {
-  clearTimeout(pingTimer)
-  pingTimer = setTimeout(() => {
-    service.get('/auth/ping').then()
-    resetPingTimer()
-  }, TOKEN_REFRESH_EXPIRE * 60 * 1000)
-}
-
-/**
- * 接口数据
- */
-interface ApiData<T> extends Record<string, unknown> {
-  code: number
-  msg: string
-  data?: T
-  page?: Pagination
-}
-
-/**
- * 分页
- */
-interface Pagination {
-  pageIndex: number
-  pageSize: number
-  totalCount: number
-}
-
-/**
- * 请求拆包
- * @param request 请求
- */
-function unpack<T>(request: Promise<AxiosResponse<ApiData<T>>>): Promise<ApiData<T>> {
-  return new Promise((resolve, reject) => {
-    request
-      .then((res) => {
-        // 操作成功时（code = 0）【其他情况自行调整】
-        if (res.data.code === 0) {
-          resolve(res.data)
-        } else {
-          reject(res.data)
+) => {
+  const axiosConfigIn = merge(DEFAULT_AXIOS_CONFIG, axiosConfig)
+  const instance = axios.create()
+  instance.interceptors.request.use((config) => {
+    config.headers.tokenId = getTokenAUTH()
+    switch (method) {
+      case 'get':
+        config.params = params
+        // 如果要在get请求设置content-Type
+        if (axiosConfig?.headers?.contentType) {
+          // 未设置requestData的时候源码会删掉Content-Type的设置，其这么做的原因在于是Get请求本身是不需要Content-Type的。
+          config.data = true
         }
-      })
-      .catch((err) => {
-        reject(err)
-      })
+        break
+      case 'post':
+        config.headers['Content-Type'] =
+          axiosConfig?.headers?.contentType || contentTypes[paramsType]
+        // http://axios-js.com/zh-cn/docs/#%E4%BD%BF%E7%94%A8-application-x-www-form-urlencoded-format
+        config.data = paramsType === 'urlencoded' ? QS.stringify(params) : params
+        break
+      default:
+        break
+    }
+    if (!repeat_request) {
+      removePending(config) // 重复请求相关
+      addPending(config)
+    }
+    if (loading) {
+      LoadingInstance._count++
+      if (LoadingInstance._count === 1) {
+        LoadingInstance._target = ElLoading.service(loadingOptions)
+      }
+    }
+    // 移除起始部分 / 所有请求url走相对路径
+    config.url = config.url.replace(/^\//, '')
+    return config
+  })
+
+  instance.interceptors.response.use(
+    (res) => {
+      httpErrorStatusHandle(res)
+      !repeat_request && removePending(res.config)
+      loading && closeLoading(loading) // 关闭loading
+      return Promise.resolve(res)
+    },
+    (error) => {
+      httpErrorStatusHandle(error)
+      error.config && !repeat_request && removePending(error.config)
+      loading && closeLoading(loading) // 关闭loading
+      return Promise.reject(error) // 错误继续返回给到具体页面
+    }
+  )
+  return instance({
+    url: IS_USE_GLOBAL_MOCK
+      ? `${MOCK_SERVER_IP}/${url}`
+      : `${prefixUrl}`
+      ? `${prefixUrl}/${url}`
+      : `${DEFAULT_PREFIX_URL}/${url}`,
+    method,
+    ...axiosConfigIn,
   })
 }
 
-const api = {
-  get<T>(url: string, params?: unknown) {
-    return unpack(service.get<ApiData<T>, AxiosResponse<ApiData<T>>, unknown>(url, { params }))
-  },
-  post<T>(url: string, data?: unknown) {
-    return unpack(
-      service.post<ApiData<T>, AxiosResponse<ApiData<T>>, unknown>(url, JSON.stringify(data), {
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8',
-        },
-      })
-    )
-  },
-  put<T>(url: string, data?: unknown) {
-    return unpack(
-      service.put<ApiData<T>, AxiosResponse<ApiData<T>>, unknown>(url, JSON.stringify(data), {
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8',
-        },
-      })
-    )
-  },
-  patch<T>(url: string, data?: unknown) {
-    return unpack(
-      service.patch<ApiData<T>, AxiosResponse<ApiData<T>>, unknown>(url, JSON.stringify(data), {
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8',
-        },
-      })
-    )
-  },
-  delete<T>(url: string, params?: unknown) {
-    return unpack(
-      service.delete<ApiData<T>, AxiosResponse<ApiData<T>>, unknown>(url, {
-        params,
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest',
-          'Content-Type': 'application/json;charset=UTF-8',
-        },
-        withCredentials: true,
-      })
-    )
-  },
-  /**
-   * 上传文件接口
-   *
-   * @param url
-   * @param formData
-   */
-  upload<T>(url: string, formData: FormData) {
-    return unpack(service.post<ApiData<T>, AxiosResponse<ApiData<T>>, unknown>(url, formData))
-  },
-  /**
-   * 下载文件
-   *
-   * @param url
-   * @param params
-   */
-  download(url: string, params?: unknown) {
-    return new Promise<ApiData<ArrayBuffer>>((resolve, reject) => {
-      service
-        .get<ArrayBuffer, AxiosResponse<ArrayBuffer>, unknown>(url, {
-          responseType: 'arraybuffer',
-          params,
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/json;charset=UTF-8',
-          },
-          withCredentials: true,
-        })
-        .then((res) => {
-          if (res.headers.filename) {
-            resolve({
-              data: res.data,
-              filename: decodeURI(res.headers.filename),
-              code: parseInt(res.headers['code'] || '0'),
-              msg: decodeURI(res.headers['msg'] || ''),
-            })
-          } else {
-            reject(res.data)
-          }
-        })
-        .catch((err) => resolve(err))
+/**
+ * 处理异常
+ * @param {*} error
+ */
+const httpErrorStatusHandle = (error) => {
+  let message
+  if (error?.response) {
+    const msgByCode = new Map([
+      [302, '接口重定向'],
+      [400, '参数不正确！'],
+      [401, '您未登录，或者登录已经超时，请先登录！'], // TODO跳转登录页
+      [403, '您没有权限操作！'],
+      // [404, `请求地址出错: ${error?.response?.config?.url}`], // 在正确域名下
+      [408, '请求超时！'],
+      [409, '系统已存在相同数据！'],
+      [500, '服务器内部错误！'],
+      [501, '服务未实现！'],
+      [502, '网关错误！'],
+      [503, '服务不可用！'],
+      [504, '服务暂时无法访问，请稍后再试！'],
+      [505, 'HTTP版本不受支持！'],
+    ])
+    message = msgByCode.get(error?.response?.status) || '异常问题，请联系管理员！'
+  }
+  if (axios.isCancel(error)) return console.error('请求的重复请求：' + error.message)
+  if (error?.message?.includes('timeout')) message = '网络请求超时！'
+  if (error?.message?.includes('Network'))
+    message = window.navigator.onLine ? '服务端异常！' : '您断网了！'
+  message &&
+    ElMessage({
+      type: 'error',
+      message,
     })
-  },
+}
+/**
+ * 关闭Loading层实例
+ * @param {*} _options
+ */
+const closeLoading = (loading) => {
+  if (loading && LoadingInstance._count > 0) LoadingInstance._count--
+  if (LoadingInstance._count === 0) {
+    LoadingInstance._target.close()
+    LoadingInstance._target = null
+  }
+}
+/**
+ * 生成每个请求唯一的键
+ * @param {*} config
+ * @returns string
+ */
+const getPendingKey = (config) => {
+  let { url, method, params, data } = config
+  data = typeof data === 'object' ? JSON.stringify(data) : '' + data
+  return [url, method, JSON.stringify(params), data].join('&')
+}
+/**
+ * 储存每个请求唯一值, 也就是cancel()方法, 用于取消请求
+ * @param {*} config
+ */
+const addPending = (config) => {
+  const pendingKey = getPendingKey(config)
+  console.log(pendingKey, 'pendingKey')
+  config.cancelToken =
+    config.cancelToken ||
+    new axios.CancelToken((cancel) => {
+      if (!pendingRequestMap.has(pendingKey)) {
+        pendingRequestMap.set(pendingKey, cancel)
+      }
+    })
+}
+/**
+ * 删除重复的请求
+ * @param {*} config
+ */
+const removePending = (config) => {
+  const pendingKey = getPendingKey(config)
+  if (pendingRequestMap.has(pendingKey)) {
+    const cancelToken = pendingRequestMap.get(pendingKey)
+    cancelToken(pendingKey)
+    pendingRequestMap.delete(pendingKey)
+  }
 }
 
-export { BASE_URL as baseURL, service as axios, api }
+export const get = (url, arg) => fetch(url, { ...arg, method: 'get' })
+export const post = (url, arg) => fetch(url, { ...arg, method: 'post' })
